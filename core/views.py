54eaacd56575846e9353491dc817912046d043d2
EGAMAGZ <gazapito1@hotmail.com>
2019-12-20 23:56:01 -0600

    Added more security to core/views.py

--- core/views.py
+++ core/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt,csrf_protect
+from django.views.decorators.clickjacking import xframe_options_deny
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
 from .form import LoginForm
 
 # Create your views here.
-
+@csrf_exempt
 def home(request):
     return render(request,'core/home.html')
 
+@csrf_protect
+@xframe_options_deny
 def login(request):
     if(request.method=="POST"):
         form=LoginForm(request.POST)
@@ -17,8 +21,12 @@ def login(request):
         form=LoginForm()
     return render(request,'core/login.html',{'form':form})
 
+@csrf_protect
+@xframe_options_deny
 def register(request):
     return render(request,'core/register.html')
 
+@csrf_protect
+@xframe_options_deny
 def contact(request):
     return render(request,'core/contact.html')
