1ed833742d1a3599e7439ae4a4a1dcee63729ec8
EGAMAGZ <gazapito1@hotmail.com>
2019-12-20 23:34:19 -0600

    Redesigned the login.html and access.html template (to make it similar to microsoft outlook login). Created and wroten code on login.css.Modified main.css.Added a new photo.Created login.js and wroten code (some validation and DOM modification)

---
+++ core/static/core/js/login.js
@@ -0,0 +1,21 @@
+var forms=document.getElementsByClassName("form-group");
+var next_btn=document.getElementById('next-btn');
+
+var input_email=document.getElementById("id_ad_email");
+
+var errors_email=document.getElementById("id_ad_email_errors");
+
+next_btn.onclick=function(){
+    let email=input_email.value;
+    if(validateEmail(email)){
+        forms[0].style.display="none";
+        forms[1].style.display="block";
+    }else{
+        errors_email.innerHTML="<ul><li>Enter a valid email address.</li></ul>";
+    }
+};
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
