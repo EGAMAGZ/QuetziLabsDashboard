be826f116ccf7bef444e597abacde944b972d8b7
EGAMAGZ <gazapito1@hotmail.com>
2019-12-27 19:48:58 -0600

    Added another table ql_free_dt

--- core/models.py
+++ core/models.py
@@ -4,5 +4,11 @@ from django.db import models
 class QL_USR_DT(models.Model):
     ql_usr_img_link=models.ImageField(upload_to="user-profile-picture")
     ql_usr_name=models.CharField(max_length=60)
-    ql_usr_email=models.EmailField(max_length=254)
+    ql_usr_email=models.EmailField()#By default the max_Length is 254
     ql_usr_username=models.CharField(max_length=30)
+
+class QL_FREE_DT(models.Model):
+    ql_usr_img_link=models.ImageField(upload_to="freelance-profile-picture")
+    ql_free_name=models.CharField(max_length=60)
+    ql_free_email=models.EmailField(max_length=254)
+    ql_free_desc=models.TextField(max_length=500)
