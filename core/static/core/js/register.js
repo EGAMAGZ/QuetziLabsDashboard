2264fa348c664a5fea1b4528b0fdf6c78882f87f
EGAMAGZ <gazapito1@hotmail.com>
2019-12-29 23:14:11 -0600

    Added image format validation (register.js)

--- core/static/core/js/register.js
+++ core/static/core/js/register.js
@@ -4,14 +4,20 @@ var profile_img=document.getElementById('profile-img');
 input_img.onchange=function (evt) {
     var tgt = evt.target || window.event.srcElement,
         files = tgt.files;
+// console.log(files[0].name);
 
     // FileReader support
     if (FileReader && files && files.length) {
-        var fr = new FileReader();
-        fr.onload = function () {
-            profile_img.src = fr.result;
+        let file_name=files[0].name;
+        if(validate_fileupload(file_name)){
+            var fr = new FileReader();
+            fr.onload = function () {
+                profile_img.src = fr.result;
+            }
+            fr.readAsDataURL(files[0]);
+        }else{
+            alert("Archivo No Soportado");
         }
-        fr.readAsDataURL(files[0]);
     }
 
     // Not supported
@@ -19,4 +25,20 @@ input_img.onchange=function (evt) {
         // fallback -- perhaps submit the input to an iframe and temporarily store
         // them on the server until the user's session ends.
     }
+}
+
+function validate_fileupload(fileName)
+{
+    var allowed_extensions = new Array("jpg","png","gif");
+    var file_extension = fileName.split('.').pop().toLowerCase(); // split function will split the filename by dot(.), and pop function will pop the last element from the array which will give you the extension as well. If there will be no extension then it will return the filename.
+
+    for(var i = 0; i <= allowed_extensions.length; i++)
+    {
+        if(allowed_extensions[i]==file_extension)
+        {
+            return true; // valid file extension
+        }
+    }
+
+    return false;
 }
