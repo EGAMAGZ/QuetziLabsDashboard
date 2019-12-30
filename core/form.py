d4ade0a8cb4ecc9ddc31f92ad69157e546c79598
EGAMAGZ <gazapito1@hotmail.com>
2019-12-29 23:02:53 -0600

    Added design to register.html, and form but without responsive design and tests. Added register.js to change img when is uploaded

--- core/form.py
+++ core/form.py
@@ -13,9 +13,10 @@ class RegisterForm(forms.Form):
         ("M","Hombre"),
         ("F","Mujer"),
     )
-    ad_profile=forms.ImageField(label="profile image",validators=[validators.validate_image_file_extension,],required=False)
-    ad_name=forms.CharField(label="name",max_length=60,min_length=15,required=True)
-    ad_email=forms.EmailField(label="email",widget=forms.EmailInput(attrs={'class':'form-control','placeholder': 'Correo Electrónico'}),required=True,validators=[validators.validate_email,])
+    ad_profile=forms.ImageField(label="profile image",widget=forms.FileInput(attrs={'class':'form-control-file',}),validators=[validators.validate_image_file_extension,],required=False)
+    ad_name=forms.CharField(label="name",max_length=60,min_length=15,widget=forms.TextInput(attrs={'class':'form-control',}),required=True)
+    ad_email=forms.EmailField(label="email",widget=forms.EmailInput(attrs={'class':'form-control','placeholder': 'ejemplo@correo.com'}),required=True,validators=[validators.validate_email,])
     ad_password=forms.CharField(label="password",widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Contraseña'}),required=True,min_length=6)
-    ad_username=forms.CharField(min_length=6,max_length=30,required=True)
-    ad_gender=forms.ChoiceField(choices=GENDER,required=True)
+    ad_password_s=forms.CharField(label="password",widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Confirmar Contraseña'}),required=True,min_length=6)
+    ad_username=forms.CharField(min_length=6,max_length=30,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'user1234'}),required=True)
+    ad_genre=forms.ChoiceField(choices=GENDER,widget=forms.Select(attrs={'class':'form-control w-auto'}),required=True)
---
+++ core/static/core/js/register.js
@@ -0,0 +1,22 @@
+var input_img=document.getElementById('id_ad_profile');
+var profile_img=document.getElementById('profile-img');
+
+input_img.onchange=function (evt) {
+    var tgt = evt.target || window.event.srcElement,
+        files = tgt.files;
+
+    // FileReader support
+    if (FileReader && files && files.length) {
+        var fr = new FileReader();
+        fr.onload = function () {
+            profile_img.src = fr.result;
+        }
+        fr.readAsDataURL(files[0]);
+    }
+
+    // Not supported
+    else {
+        // fallback -- perhaps submit the input to an iframe and temporarily store
+        // them on the server until the user's session ends.
+    }
+}
