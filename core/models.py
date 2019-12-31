0100eb89a3749fd5664d27560a11ea64c5d5d76e
EGAMAGZ <gazapito1@hotmail.com>
2019-12-30 22:58:22 -0600

    Increase the length for the normal user password

--- core/models.py
+++ core/models.py
@@ -9,7 +9,7 @@ class QL_USR_DT(models.Model):
     ql_usr_img_link=models.ImageField(upload_to="user-profile-picture")
     ql_usr_name=models.CharField(max_length=60)
     ql_usr_email=models.EmailField(unique=True)#By default the max_Length is 254
-    ql_usr_pass=models.CharField(max_length=64)#hash256 max_length=64 bcrypt hash256 lenght=50
+    ql_usr_pass=models.CharField(max_length=254)#hash256 max_length=64 bcrypt hash256 lenght=50
     ql_usr_username=models.CharField(max_length=30,unique=True)
     ql_usr_gender = models.CharField(max_length=1, choices=GENDER, default="M")
 
